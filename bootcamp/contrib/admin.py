8fae05e490bed18f077212c5979dc64498c3af2f
hamza <hamza.bakouchi@gmail.com>
2021-12-30 18:28:13 +0100

    content field as summernote field

--- bootcamp/contrib/admin.py
+++ bootcamp/contrib/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.contrib.flatpages.admin import FlatPageAdmin
+from django_summernote.admin import SummernoteModelAdmin
 from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import gettext_lazy as _
 
 # Define a new FlatPageAdmin
-class FlatPageAdmin(FlatPageAdmin):
+class FlatPageAdmin(FlatPageAdmin, SummernoteModelAdmin):
+    summernote_fields = ('content',)
     fieldsets = (
         (None, {'fields': ('url', 'title', 'content', 'sites')}),
         (_('Advanced options'), {
