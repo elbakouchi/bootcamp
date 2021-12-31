fee66744720627bcafa5eb8997b58b0ee7d01e7b
hamza <hamza.bakouchi@gmail.com>
2021-12-31 01:59:10 +0100

    summernote as widget

--- bootcamp/contrib/admin.py
+++ bootcamp/contrib/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
 from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.forms import FlatPageForm
+#from django.contrib.flatpages.models import FlatPage
 from django_summernote.admin import SummernoteModelAdmin
+from django_summernote.widgets import SummernoteInplaceWidget
 from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import gettext_lazy as _
 
+
+
+class CustomFlatpageForm(FlatpageForm):
+    class Meta:
+        model = FlatPage
+        fields = '__all__'
+        widgets = {
+            'content': SummernoteInplaceWidget(),
+        }
+
 # Define a new FlatPageAdmin
-class FlatPageAdmin(SummernoteModelAdmin, FlatPageAdmin):
+class FlatPageAdmin(FlatPageAdmin):
+    form = CustomFlatpageForm
     summernote_fields = ('content',)
     fieldsets = (
         (None, {'fields': ('url', 'title', 'content', 'sites')}),
