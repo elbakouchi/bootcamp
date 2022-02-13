0ad8d07e5cdcdb05c72686f1b727d6abace2ee85
hamza <hamza.bakouchi@gmail.com>
2022-02-13 12:29:41 +0100

    fix list

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,7 +86,7 @@ LOCAL_APPS = [
     "bootcamp.users.apps.UsersConfig",
     # Your stuff: custom apps go here
     "bootcamp.articles.apps.ArticlesConfig",
-    "bootcamp.category.apps.CategoryConfig"
+    "bootcamp.category.apps.CategoryConfig",
     "bootcamp.demand.apps.DemandsConfig",
     "bootcamp.messager.apps.MessagerConfig",
     "bootcamp.news.apps.NewsConfig",
