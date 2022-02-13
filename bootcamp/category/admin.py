3c156dd8258d614a1fbf731a6b791a601de79aeb
hamza <hamza.bakouchi@gmail.com>
2022-02-13 18:51:40 +0100

    add services to admin

--- bootcamp/category/admin.py
+++ bootcamp/category/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Service
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "user", "activated")
     list_filter = ("user", "activated", "timestamp")
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "user", "activated")
+    list_filter = ("user", "activated", "timestamp")
+
+
