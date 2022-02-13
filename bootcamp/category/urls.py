1fffe49da3f74e0e4a917fb7365196f6ebee2fe5
hamza <hamza.bakouchi@gmail.com>
2022-02-14 00:38:06 +0100

    fix indentation

--- bootcamp/category/urls.py
+++ bootcamp/category/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
-from . import views
+from .views import CategoriesListView, CategoryDetailView
 
 app_name = "categories"
 urlpatterns = [
-    url(regex=r"^$", view=views.CategoriesListView.as_view(), name="list"),
-    #url(regex=r"^~redirect/$", view=views.UserRedirectView.as_view(), name="redirect"),
-    #url(regex=r"^~update/$", view=views.UserUpdateView.as_view(), name="update"),
+    url(regex=r"^$", view=CategoriesListView.as_view(), name="list"),
+    #url(regex=r"^~redirect/$", view=UserRedirectView.as_view(), name="redirect"),
+    #url(regex=r"^~update/$", view=UserUpdateView.as_view(), name="update"),
     url(
         regex=r"^(?P<slug>[\w.@+-]+)/$",
-        view=views.CategoryDetailView.as_view(),
+        view=CategoryDetailView.as_view(),
         name="detail",
     ),
 ]
