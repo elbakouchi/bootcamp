24366189c5be97f9739c5e1f7aa5c49620cbe037
hamza <hamza.bakouchi@gmail.com>
2022-02-14 00:23:33 +0100

    imports

--- bootcamp/category/views.py
+++ bootcamp/category/views.py
@@ -8,6 +8,8 @@ class CategoriesListView(ListView):
     # These next two lines tell the view to index lookups by username
     slug_field = "slug"
     slug_url_kwarg = "slug"
+     def get_queryset(self, **kwargs):
+        return Category.objects.get_activated()
 
 class CategoryDetailView(DetailView):
     model = Category
