7a0faaddd2df2ca54220ac2bdb35da5ba588100c
hamza <hamza.bakouchi@gmail.com>
2022-02-14 00:22:34 +0100

    list and detail routes

--- bootcamp/category/urls.py
+++ bootcamp/category/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 app_name = "categories"
 urlpatterns = [
-    url(regex=r"^$", view=views.UserListView.as_view(), name="list"),
+    url(regex=r"^$", view=views.CategoriesListView.as_view(), name="list"),
     #url(regex=r"^~redirect/$", view=views.UserRedirectView.as_view(), name="redirect"),
     #url(regex=r"^~update/$", view=views.UserUpdateView.as_view(), name="update"),
     url(
         regex=r"^(?P<slug>[\w.@+-]+)/$",
-        view=views.UserDetailView.as_view(),
+        view=views.CategoryDetailView.as_view(),
         name="detail",
     ),
 ]
