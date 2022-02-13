6ec1b3a1c29d05a7bf16c728bc6d1be7b949798b
hamza <hamza.bakouchi@gmail.com>
2022-02-13 12:27:28 +0100

    import lazy text

--- bootcamp/category/apps.py
+++ bootcamp/category/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class CategoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
