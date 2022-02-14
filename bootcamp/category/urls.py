ec7c88b7a40c88c6252a36902fbaecf4eb5fd42d
hamza <hamza.bakouchi@gmail.com>
2022-02-14 13:44:58 +0100

    rearrage code

--- bootcamp/category/urls.py
+++ bootcamp/category/urls.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import url
-from bootcamp.category.views import CategoriesListView, CategoryDetailView
+from bootcamp.category.views import CategoriesListView, \
+    CategoryDetailView
 
-app_name = "categories"
+app_name = 'categories'
 
-urlpatterns = [
-    url(regex=r"^$", view=CategoriesListView.as_view(), name="list"),
-    url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryDetailView.as_view(), name="detail")
-]
+urlpatterns = [url(regex=r"^$", view=CategoriesListView.as_view(),
+               name='list'), url(regex=r"^(?P<slug>[\w.@+-]+)/$",
+               view=CategoryDetailView.as_view(), name='detail')]
