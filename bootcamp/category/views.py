ea2f3913edba3485f780f608b08c52ae69021b7b
hamza <hamza.bakouchi@gmail.com>
2022-02-14 14:35:34 +0100

    remove useless import, reformat file

--- bootcamp/category/views.py
+++ bootcamp/category/views.py
@@ -1,17 +1,19 @@
-from django.views.generic import DetailView, ListView, RedirectView, UpdateView
-
+from django.views.generic import DetailView, ListView
 from .models import Category
+
 
 class CategoriesListView(ListView):
     model = Category
     # These next two lines tell the view to index lookups by username
     slug_field = "slug"
     slug_url_kwarg = "slug"
-     def get_queryset(self, **kwargs):
+
+    def get_queryset(self, **kwargs):
         return Category.objects.get_activated()
+
 
 class CategoryDetailView(DetailView):
     model = Category
     # These next two lines tell the view to index lookups by username
     slug_field = "slug"
-    slug_url_kwarg = "slug"    
+    slug_url_kwarg = "slug"
