fea5c52ebfd49840101f7c13636f2ac7a6f6d9fa
hamza <hamza.bakouchi@gmail.com>
2022-02-15 23:11:50 +0100

    one parent only

--- bootcamp/home/views.py
+++ bootcamp/home/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 from bootcamp.category.views import CategoriesListView
 
 
-class HomePageView(TemplateView, CategoriesListView):
+class HomePageView(CategoriesListView):
     template_name = 'redico/homepage.html'
 
     def get_context_data(self, *args, **kwargs):
