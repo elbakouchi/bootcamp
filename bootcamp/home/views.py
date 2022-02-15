76d0484f7aa37e44fdc4ba367152b146c117460b
hamza <hamza.bakouchi@gmail.com>
2022-02-15 22:06:49 +0100

    use raw class name

--- bootcamp/home/views.py
+++ bootcamp/home/views.py
@@ -6,7 +6,7 @@ class HomePageView(TemplateView, CategoriesListView):
     template_name = 'redico/homepage.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['categories'] = super().get_queryset(**kwargs)
+        context = CategoriesListView.get_context_data(*args, **kwargs)
+        context['categories'] = CategoriesListView.get_queryset(**kwargs)
         return context
 
