204d181d3c2b8fc896572af176aff5c8543064d1
hamza <hamza.bakouchi@gmail.com>
2022-02-18 22:43:15 +0100

    reformat

--- bootcamp/demand/migrations/0003_add_createdat_updatedat.py
+++ bootcamp/demand/migrations/0003_add_createdat_updatedat.py
@@ -2,23 +2,22 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("demand", "0001_initial"),
         ("demand", "0002_alter_image_null"),
     ]
 
     operations = [
-    
-         migrations.AddField(
+
+        migrations.AddField(
             model_name="demand",
             name="createdAt",
             field=models.DateTimeField(auto_now_add=True),
         ),
-         migrations.AddField(
+        migrations.AddField(
             model_name="demand",
             name="updatedAt",
             field=models.DateTimeField(auto_now=True),
         )
-        
+
     ]
