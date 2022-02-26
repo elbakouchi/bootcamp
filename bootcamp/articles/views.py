b8659835d1ff766d6b3dcd9270702ebb47bdec54
hamza <hamza.bakouchi@gmail.com>
2022-02-26 09:52:12 +0100

    print out article

--- bootcamp/articles/views.py
+++ bootcamp/articles/views.py
@@ -76,5 +76,9 @@ class DetailArticleView(DetailView):
 
     def get_object(self, queryset=None):
         article = super(DetailArticleView, self).get_object()
-        return Article.objects.filter(pk=article.pk).annotate(
+        print(article)
+        article = Article.objects.filter(pk=article.pk).annotate(
                   categoryName=StringAgg('demand__category__name', delimiter=','))
+        print(vars(article))
+        return article
+
