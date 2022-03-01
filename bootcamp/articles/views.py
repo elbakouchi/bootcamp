e1fae27c58c268e24ea99772c9f5c03508af1e51
hamza <hamza.bakouchi@gmail.com>
2022-03-01 07:10:50 +0100

    get article with category

--- bootcamp/articles/views.py
+++ bootcamp/articles/views.py
@@ -3,9 +3,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, ListView, UpdateView, DetailView
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.postgres.aggregates import StringAgg
+from django.template.loader import render_to_string
+from django.core.paginator import Paginator
+from django.http import JsonResponse, HttpResponse
+# ,  HttpResponse, HttpResponseBadRequest
 
-from bootcamp.helpers import AuthorRequiredMixin
+from django.contrib.auth.decorators import login_required
+from bootcamp.helpers import ajax_required, AuthorRequiredMixin
+from django.views.decorators.http import require_http_methods
+
 from bootcamp.articles.models import Article
 from bootcamp.articles.forms import ArticleForm
 
@@ -73,4 +79,47 @@ class DetailArticleView(DetailView):
     template_name = "redico/article-single.html"
     context_object_name = 'article'
     model = Article
-    # pk_url_kwarg = 'slug'
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super(DetailArticleView, self).get_queryset()
+        return queryset.get_category()
+
+
+# @login_required
+# @ajax_required
+@require_http_methods(["GET"])
+def get_category_articles(request, pk, page, page_size):
+    """Returns a paginated list of articles with the given category id."""
+    category_id = pk if pk is not None else request.GET["category"]
+
+    articles, paginator = get_paginated_articles(category_id, page, page_size)
+
+    articles_html = render_to_string("redico/articles/paginated_articles.html", {"articles": paginator.get_page(page)})
+
+    pagination_html = render_to_string(
+        "redico/articles/articles_pagination.html", {"current": paginator.page(page),
+                                                     "page_range": paginator.page_range}
+    )
+    return JsonResponse({"category_id": category_id,
+                         "articles_html": articles_html,
+                         "pagination_html": pagination_html
+                         })
+
+
+def get_paginated_articles(category_id, page, page_size):
+    articles = Article.objects.get_category().filter(demand__category__pk=category_id)
+    paginator = Paginator(articles, page_size)
+    return articles, paginator
+
+
+def paginate(request, pk, page):
+    category_id = pk if pk is not None else request.GET["category"]
+    page = request.GET.get('page', None)
+    pk = request.GET.get('pk', None)
+    starting_number = (page - 1) * 6
+    ending_number = page * 6
+
+    result = Article.objects.get_category().filter(demand__category__pk=category_id)[starting_number:ending_number]
+    data = {result}
+
+    return JsonResponse(data)
