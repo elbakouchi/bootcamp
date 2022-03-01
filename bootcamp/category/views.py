d6554419631f3ec24bdcd32067d77f75665acaa4
hamza <hamza.bakouchi@gmail.com>
2022-03-01 07:52:00 +0100

    get activated categories only, paginate by 6 article items

--- bootcamp/category/views.py
+++ bootcamp/category/views.py
@@ -1,21 +1,38 @@
+from django.db.models import Count
 from django.views.generic import DetailView, ListView
+from django.views.generic.detail import SingleObjectMixin
+
 from .models import Category
+from ..articles.models import Article
 
 
 class CategoriesListView(ListView):
     template_name = 'redico/categories.html'
     model = Category
-    paginate_by = 15
+    paginate_by = 6
     context_object_name = "categories"
     # These next two lines tell the view to index lookups by username
     slug_field = "slug"
     slug_url_kwarg = "slug"
+
     # categories = []
 
     def get_queryset(self, **kwargs):
         # queryset = super(CategoriesListView, self).get_queryset()
         # return queryset.filter(activated=True)
-        return Category.objects.get_activated()
+        return Category.objects.get_categories_with_demands_count()
+
+
+class CategoryArticlesView(SingleObjectMixin, ListView):
+    template_name = 'redico/liste-article.html'
+    paginate_by = 6
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Category.objects.get_activated())
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return Article.objects.get_category().filter(demand__category__pk=self.object.pk)
 
 
 class CategoryDetailView(DetailView):
@@ -28,4 +45,4 @@ class CategoryDetailView(DetailView):
     # def
 
     def get_queryset(self, **kwargs):
-        return Category.objects.get_activated()
+        return Category.objects.all()
