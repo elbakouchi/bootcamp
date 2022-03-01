952359100014e300c597380c0d68a7e9e1cc2ee2
hamza <hamza.bakouchi@gmail.com>
2022-03-01 07:05:16 +0100

    category detail view url

--- bootcamp/category/urls.py
+++ bootcamp/category/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from bootcamp.category.views import CategoriesListView, CategoryDetailView
+from bootcamp.category.views import CategoriesListView, CategoryDetailView, CategoryArticlesView
 
 app_name = 'categories'
 
 urlpatterns = [
     url(regex=r"^$", view=CategoriesListView.as_view(), name='list'),
-    url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryDetailView.as_view(), name='detail')
+    # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryDetailView.as_view(), name='detail'),
+    url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryArticlesView.as_view(), name='articles')
 ]
