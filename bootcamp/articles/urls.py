38088645a3b32777f3213f7284d44aa1b9ec290a
hamza <hamza.bakouchi@gmail.com>
2022-03-01 07:04:47 +0100

    detail article view with pk

--- bootcamp/articles/urls.py
+++ bootcamp/articles/urls.py
@@ -6,6 +6,7 @@ from bootcamp.articles.views import (
     CreateArticleView,
     EditArticleView,
     DetailArticleView,
+    get_category_articles
 )
 
 app_name = "articles"
@@ -14,5 +15,8 @@ urlpatterns = [
     url(r"^write-new-article/$", CreateArticleView.as_view(), name="write_new"),
     url(r"^drafts/$", DraftsListView.as_view(), name="drafts"),
     url(r"^edit/(?P<pk>\d+)/$", EditArticleView.as_view(), name="edit_article"),
+    url(r"^(?P<pk>\d+)/$", DetailArticleView.as_view(), name="article_pk"),
     url(r"^(?P<slug>[-\w]+)/$", DetailArticleView.as_view(), name="article"),
+    url(r"^get-category-articles/(?P<pk>\d+)/(?P<page>\d+)/(?P<page_size>\d+)$", get_category_articles,
+        name="get_category_paginated_articles"),
 ]
