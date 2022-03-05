77eba540c99712df2e0ac9b16541ebdc3b72b045
hamza <hamza.bakouchi@gmail.com>
2022-03-05 21:26:55 +0100

    demand instead of article

--- bootcamp/category/urls.py
+++ bootcamp/category/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from bootcamp.category.views import CategoriesListView, CategoryArticlesView
+from bootcamp.category.views import CategoriesListView, CategoryArticlesView, CategoryDemandsView
 
 app_name = 'categories'
 
 urlpatterns = [
     url(regex=r"^$", view=CategoriesListView.as_view(), name='list'),
     # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryDetailView.as_view(), name='detail'),
-    url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryArticlesView.as_view(), name='articles')
+    # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryArticlesView.as_view(), name='articles'),
+    url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryDemandsView.as_view(), name='demands')
 ]
