cc45ce08383f4c7f40f265f5961f6232773ed058
hamza <hamza.bakouchi@gmail.com>
2022-03-05 21:29:01 +0100

    demand instead of article

--- bootcamp/home/views.py
+++ bootcamp/home/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from django.contrib.postgres.aggregates import StringAgg
 # from django.views.generic import TemplateView
 
-from bootcamp.articles.models import Article
 from bootcamp.category.models import Category
 from bootcamp.category.views import CategoriesListView
+from bootcamp.demand.models import Demand
 
 
 class HomePageView(CategoriesListView):
@@ -18,8 +18,8 @@ class HomePageView(CategoriesListView):
 
 
 def homepage(request):
-    articles = \
-        Article.objects.filter(status="P").annotate(categoryName=StringAgg('demand__category__name', delimiter=','))
-    categories = Category.objects.filter(activated=True).annotate(posts_count=Count('taxonomy_category'))
-    return render(request, 'redico/homepage.html', {'categories': categories, 'articles': articles})
+    demands = \
+        Demand.objects.filter(status="P").annotate(categoryName=StringAgg('category__name', delimiter=','))
+    categories = Category.objects.filter(activated=True).annotate(posts_count=Count('demand_category'))
+    return render(request, 'redico/homepage.html', {'categories': categories, 'articles': demands})
 
