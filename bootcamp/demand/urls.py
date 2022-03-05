bca694d6e9908d414c1d6c24cbc954bbc3ec7606
hamza <hamza.bakouchi@gmail.com>
2022-03-05 21:27:11 +0100

    demand detail

--- bootcamp/demand/urls.py
+++ bootcamp/demand/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from bootcamp.demand.views import CreateDemandView
+from bootcamp.demand.views import CreateDemandView, DetailDemandView
 
 app_name = 'demands'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     # url(regex=r"^$", view=CategoriesListView.as_view(), name='list'),
     # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryDetailView.as_view(), name='detail'),
     # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryArticlesView.as_view(), name='articles')
+    url(r"^(?P<slug>[-\w]+)/$", DetailDemandView.as_view(), name="demand"),
     url(r"^nouveau-text/$", CreateDemandView.as_view(), name="write_new")
 ]
