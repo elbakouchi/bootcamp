9fb6956454e8631fcde33a38deb5fef623d46069
hamza <hamza.bakouchi@gmail.com>
2022-03-14 21:40:14 +0100

    unfulfilled demands route

--- bootcamp/demand/urls.py
+++ bootcamp/demand/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import url
-from bootcamp.demand.views import CreateDemandView, DetailDemandView, PaginatedDemandsFeed
+from bootcamp.demand.views import CreateDemandView, DetailDemandView,\
+    PaginatedDemandsFeed, DemandsList
 
 app_name = 'demands'
 
 urlpatterns = [
+    url(r"^feed/$", PaginatedDemandsFeed.as_view(), name="demands_feed"),
+    url("^en-attente-de-reponse/$", DemandsList.as_view(), name="unfulfilled"),
     url(r"^nouveau-text/$", CreateDemandView.as_view(), name="write_new"),
     # url(regex=r"^$", view=CategoriesListView.as_view(), name='list'),
     # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryDetailView.as_view(), name='detail'),
     # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryArticlesView.as_view(), name='articles')
     url(r"^(?P<slug>[-\w]+)/$", DetailDemandView.as_view(), name="demand"),
-    url(r"^feed/$", PaginatedDemandsFeed.as_view(), name="demands_feed")
+
 
 ]
