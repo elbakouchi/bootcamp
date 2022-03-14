3abde2dcb0d1af523a9ad99e590556478aa9fb73
hamza <hamza.bakouchi@gmail.com>
2022-03-14 21:43:09 +0100

    remove useless lines

--- bootcamp/home/urls.py
+++ bootcamp/home/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 
 app_name = "home"
 urlpatterns = [
+    # path(r"feed", views.feed_pagination, name="feeds"),
+    # url(regex=r"^feed/", view=views.feed_pagination, name='demands'),
+    # url(r"^feed/$", views.feed_pagination, name="feed_pagination"),
     url(r"^$",  views.homepage, name="home"),
+
 ]
