c60d206ceee222eab29b76f8866a6f9048cefbfb
hamza <hamza.bakouchi@gmail.com>
2022-03-29 00:26:14 +0000

    add has revision to filter and list

--- bootcamp/demand/admin.py
+++ bootcamp/demand/admin.py
@@ -4,5 +4,5 @@ from bootcamp.demand.models import Demand
 
 @admin.register(Demand)
 class DemandAdmin(admin.ModelAdmin):
-    list_display = ("title", "user", "status", "verified")
-    list_filter = ("user", "status", "timestamp", "verified")
+    list_display = ("title", "user", "status", "verified", "has_revision")
+    list_filter = ("user", "status", "timestamp", "verified", "has_revision")
