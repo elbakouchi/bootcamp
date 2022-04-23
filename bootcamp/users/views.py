59933ef40a335a679bc29f6707d9f6bd1215934e
hamza <hamza.bakouchi@gmail.com>
2022-04-23 20:25:58 +0000

    user detail cbv for profile page

--- bootcamp/users/views.py
+++ bootcamp/users/views.py
@@ -1,16 +1,70 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
-
+from django.views.generic.edit import ModelFormMixin
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import User
+from django.db.models import Count, F
+from itertools import chain
+from ..articles.models import Article
+from ..demand.models import Demand
 
 
-class UserDetailView(LoginRequiredMixin, DetailView):
+class UserDetailView(LoginRequiredMixin, ModelFormMixin, DetailView):
     model = User
     # These next two lines tell the view to index lookups by username
     slug_field = "username"
     slug_url_kwarg = "username"
     template_name = 'redico/profile.html'
+    fields = ['first_name', 'last_name', 'phone', 'email', 'bio', 'picture']
+    success_url = "/users/{username}/"
+
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data()
+        demands = Demand.objects.filter(user=self.object.pk).order_by('pk', 'timestamp').annotate(service_name=F('service__name'),
+            revision_count=Count('revision__id', None))
+        context["demands_count"] = demands.count()
+
+        page = self.request.GET.get("page", 1)
+        paginator = Paginator(demands, 5)
+
+        try:
+            paginated_demands = paginator.page(page)
+        except PageNotAnInteger:
+            paginated_demands = paginator.page(1)
+        except EmptyPage:
+            paginated_demands = paginator.page(paginator.num_pages)
+
+        revisions = Article.objects.filter(user=self.object.pk).order_by('pk', 'timestamp')
+        context["revisions_count"] = revisions.count()
+        paginator2 = Paginator(revisions, 5)
+
+        try:
+            paginated_revisions = paginator2.page(page)
+        except PageNotAnInteger:
+            paginated_revisions = paginator2.page(1)
+        except EmptyPage:
+            paginated_revisions = paginator2.page(paginator2.num_pages)
+
+        context["revisions"] = paginated_revisions
+        context["demands"] = paginated_demands
+        '''
+        combined = chain(demands, revisions)
+        paginator3 = Paginator(combined, 5)
+
+        try:
+            paginated_combined = paginator3.page(page)
+        except PageNotAnInteger:
+            paginated_combined = paginator3.page(1)
+        except EmptyPage:
+            paginated_combined = paginator3.page(paginator.num_pages)
+
+        context["combined"] = paginated_combined
+        '''
+        return context
+
+    def get_success_url(self):
+        return reverse("users:detail", kwargs={"username": self.request.user.username})
 
 
 class UserRedirectView(LoginRequiredMixin, RedirectView):
@@ -22,8 +76,10 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     fields = [
-        "name",
+        "first_name",
+        "last_name",
         "email",
+        "phone",
         "picture",
         "job_title",
         "location",
