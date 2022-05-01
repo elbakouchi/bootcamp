ec6bf858315a2bdf843f92781f26e1574f6f75d2
hamza <hamza.bakouchi@gmail.com>
2022-05-01 16:59:59 +0000

    consume notification ws

--- config/asgi.py
+++ config/asgi.py
@@ -5,8 +5,27 @@ defined in the ASGI_APPLICATION setting.
 
 import os
 import django
-from channels.routing import get_default_application
+from django.core.asgi import get_asgi_application
+from django.conf.urls import url
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from bootcamp.notifications.consumers import NotificationsConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 django.setup()
-application = get_default_application()
+# application = get_default_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    [
+                        url(r"^notifications/$", NotificationsConsumer),
+                    ]
+                )
+            )
+        )
+})
+
