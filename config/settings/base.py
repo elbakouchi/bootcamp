55558c182e67a4c770adf260bd40ceedc7fd1b07
hamza <hamza.bakouchi@gmail.com>
2022-05-14 19:13:17 +0100

    fb and google oauth

--- config/settings/base.py
+++ config/settings/base.py
@@ -398,3 +398,28 @@ VERSATILEIMAGEFIELD_SETTINGS = {
     "create_images_on_demand": DEBUG,
     # 'placeholder_directory_name': 'pl'
 }
+
+SOCIALACCOUNT_PROVIDERS = {
+    'facebook': {
+        'METHOD': 'oauth2',
+        'SDK_URL': '//connect.facebook.net/fr_FR/sdk.js',
+        'SCOPE': ['email', 'public_profile'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'first_name',
+            'last_name',
+            'middle_name',
+            'name',
+            'name_format',
+            'picture',
+            'short_name'
+        ],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': 'path.to.callable',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v13.0',
+    }
+}
+
