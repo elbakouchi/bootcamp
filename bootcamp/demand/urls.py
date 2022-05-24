25202313a5f9657f776775f3e509e2fe62b277a1
hamza <hamza.bakouchi@gmail.com>
2022-05-24 23:34:28 +0100

    change fn name

--- bootcamp/demand/urls.py
+++ bootcamp/demand/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 from bootcamp.demand.views import CreateDemandView, DetailDemandView, \
     PaginatedDemandsFeed, DemandsList, PaginatedDemandsHomeFeed, \
-    EditDemandView, demandRedirect
-
+    EditDemandView, demand_redirect
 app_name = 'demands'
 
 
@@ -15,7 +14,7 @@ urlpatterns = [
     # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryDetailView.as_view(), name='detail'),
     # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryArticlesView.as_view(), name='articles')
     url(r"^(?P<slug>[-\w]+)/$", DetailDemandView.as_view(), name="demand"),
-    url(r"^redirection/(?P<pk>\d+)/$", demandRedirect, name="redirect_demand"),
+    url(r"^redirection/(?P<pk>\d+)/$", demand_redirect, name="redirect_demand"),
     url(r"^editer/(?P<pk>\d+)/$", EditDemandView.as_view(), name="edit_demand")
 ]
 
