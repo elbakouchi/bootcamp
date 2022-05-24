2a0c75b964ba503d7fa73ad6c3c7196dffafa385
hamza <hamza.bakouchi@gmail.com>
2022-05-24 23:35:07 +0100

    change route

--- bootcamp/demand/urls.py
+++ bootcamp/demand/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryDetailView.as_view(), name='detail'),
     # url(regex=r"^(?P<slug>[\w.@+-]+)/$", view=CategoryArticlesView.as_view(), name='articles')
     url(r"^(?P<slug>[-\w]+)/$", DetailDemandView.as_view(), name="demand"),
-    url(r"^redirection/(?P<pk>\d+)/$", demand_redirect, name="redirect_demand"),
+    url(r"^r/(?P<pk>\d+)/$", demand_redirect, name="redirect_demand"),
     url(r"^editer/(?P<pk>\d+)/$", EditDemandView.as_view(), name="edit_demand")
 ]
 
