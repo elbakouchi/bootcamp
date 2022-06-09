2c7a0cd2ddb7632acb3009f94d728792ddf9644f
hamza <hamza.bakouchi@gmail.com>
2022-06-09 01:51:31 +0100

    admin soft delete

--- bootcamp/articles/admin.py
+++ bootcamp/articles/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from bootcamp.articles.models import Article
+from safedelete.admin import SafeDeleteAdmin, SafeDeleteAdminFilter, highlight_deleted
 
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("title", "user", "status")
-    list_filter = ("user", "status", "timestamp")
+class ArticleAdmin(SafeDeleteAdmin):
+    list_display = (highlight_deleted, "highlight_deleted_field", "title", "user", "status")\
+                   + SafeDeleteAdmin.list_display
+    list_filter = ("user", "status", "timestamp", SafeDeleteAdminFilter,) + SafeDeleteAdmin.list_filter
+    field_to_highlight = "id"
+
+
+ArticleAdmin.highlight_deleted_field.short_description = ArticleAdmin.field_to_highlight
+
+'''
+
+
+ list_display = (highlight_deleted, "highlight_deleted_field", "first_name", "last_name", "email") + SafeDeleteAdmin.list_display
+...    list_filter = ("last_name", SafeDeleteAdminFilter,) + SafeDeleteAdmin.list_filter
+...
+...    field_to_highlight = "id"
+...
+... ContactAdmin.highlight_deleted_field.short_description = ContactAdmin.field_to_highlight
+
+'''
--- bootcamp/demand/admin.py
+++ bootcamp/demand/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from bootcamp.demand.models import Demand
+from safedelete.admin import SafeDeleteAdmin, SafeDeleteAdminFilter, highlight_deleted
 
 
 @admin.register(Demand)
-class DemandAdmin(admin.ModelAdmin):
-    list_display = ("title", "user", "status", "verified", "has_revision")
-    list_filter = ("user", "status", "timestamp", "verified", "has_revision")
+class DemandAdmin(SafeDeleteAdmin):
+    list_display = ("title", "user", "status", "verified", "has_revision") \
+                   + SafeDeleteAdmin.list_display
+    list_filter = ("user", "status", "timestamp", "verified", "has_revision", SafeDeleteAdminFilter,) \
+                  + SafeDeleteAdmin.list_filter
+
+    field_to_highlight = "id"
+
+
+DemandAdmin.highlight_deleted_field.short_description = DemandAdmin.field_to_highlight
