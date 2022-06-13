a7011692a493a1528c597ef0d0eaf688f7385e18
hamza <hamza.bakouchi@gmail.com>
2022-06-13 21:38:33 +0100

    fix update syntax

--- config/forms.py
+++ config/forms.py
@@ -5,5 +5,5 @@ class CustomContactForm(ContactForm):
 
     def get_message_dict(self) -> StringKeyedDict:
         message_dict = super(CustomContactForm, self).get_message_dict()
-        message_dict.update('html_message', True)
+        message_dict.update({'html_message': True})
         return message_dict
