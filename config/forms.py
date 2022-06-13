8c698969936035c2796ab7622a37a9a507130da6
hamza <hamza.bakouchi@gmail.com>
2022-06-13 22:15:27 +0100

    add html message to message object

--- config/forms.py
+++ config/forms.py
@@ -1,9 +1,20 @@
 from contact_form.forms import ContactForm, StringKeyedDict
+from django.template import loader
 
 
 class CustomContactForm(ContactForm):
+    def message_html(self) -> str:
+        """
+        Render the body of the message to a string.
+
+        """
+        template_name = 'contact_form/contact_form.html'
+        return loader.render_to_string(
+            template_name, self.get_context(), request=self.request
+        )
 
     def get_message_dict(self) -> StringKeyedDict:
         message_dict = super(CustomContactForm, self).get_message_dict()
-        message_dict.update({'html_message': True})
+        html_message = self.message_html()
+        message_dict.update({'html_message': html_message})
         return message_dict
