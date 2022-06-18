5ac2e66b16611d17afb425990d3e1f92989c652a
hamza <hamza.bakouchi@gmail.com>
2022-06-18 23:08:38 +0100

    show revision beneath their demand

--- bootcamp/demand/admin.py
+++ bootcamp/demand/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from bootcamp.demand.models import Demand
+from bootcamp.articles.models import Article
 from safedelete.admin import SafeDeleteAdmin, SafeDeleteAdminFilter, highlight_deleted
+
+
+class RevisionInline(admin.TabularInline):
+    extra = 1
+    model = Article
 
 
 @admin.register(Demand)
 class DemandAdmin(SafeDeleteAdmin):
+    inlines = [RevisionInline]
     list_display = (highlight_deleted, "highlight_deleted_field", "title", "user", "status", "verified", "has_revision") \
                    + SafeDeleteAdmin.list_display
     list_filter = ("user", "status", "timestamp", "verified", "has_revision", SafeDeleteAdminFilter,) \
