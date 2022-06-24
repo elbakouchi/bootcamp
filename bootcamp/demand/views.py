6bce8843bece7160160a0bf9419a737fe877c86c
hamza <hamza.bakouchi@gmail.com>
2022-06-24 01:04:21 +0100

    pagninate orphans

--- bootcamp/demand/views.py
+++ bootcamp/demand/views.py
@@ -118,6 +118,7 @@ class EditDemandView(LoginRequiredMixin, AuthorRequiredMixin, UpdateView):
 class CorrectedDemandsView(ListView):
     model = Demand
     paginate_by = 5
+    paginate_orphans = 1
     context_object_name = "demands"
     template_name = 'redico/homepage.html'
     queryset = Demand.objectz.homepage()
