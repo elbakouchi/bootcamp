70bf25f2d07bd2e49df9c6750ebeeca7ce17e7f3
Mohamed Hamza El Bakouchi <hamza.bakouchi@gmail.com>
2022-12-28 22:09:07 +0100

    return loaded template

--- bootcamp/users/views.py
+++ bootcamp/users/views.py
@@ -160,10 +160,10 @@ class ChangePasswordView(LoginRequiredMixin, UpdateView):
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
-        return reverse("users:detail", kwargs={"username": self.request.user.username})    
-        """return loader.render_to_string(
+        #return reverse("users:detail", kwargs={"username": self.request.user.username})    
+        return loader.render_to_string(
             self.template_name, self.get_context_data(), request=self.request
-        )""" 
+        ) 
 
     def get_context_data(self, **kwargs):
         try:
